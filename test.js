9fb704e4a3e47f32209315fe164c89aa14b00719
Sergey Ermakovich <s.ermakovich@gmail.com>
2018-07-19 13:32:36 +0300

    disable sandbox for puppeteer due to issues in docker

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ import puppeteer from 'puppeteer';
 import mockServer from '.';
 
 test('inits normally with Puppeteer', async () => {
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
 
   try {
