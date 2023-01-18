23878b8e390a84a3d9b6deac1352f073092d3d10
Sergey Ermakovich <s.ermakovich@gmail.com>
2023-01-18 21:21:04 +0300

    remove dev-configs (#217)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  extends: [
-    './node_modules/dev-configs/.eslintrc.js',
-    'plugin:jest/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:jest/recommended'],
   env: {
     commonjs: true,
     'jest/globals': true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   plugins: ['jest'],
   rules: {
     'no-console': 'off',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', {allowTemplateLiterals: true}],
+    semi: ['error', 'always'],
   },
 };
