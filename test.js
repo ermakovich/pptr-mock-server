89460df5f41bba6219ba8c37101f952568662a74
Sergey Ermakovich <s.ermakovich@gmail.com>
2026-08-30 23:19:36 +0300

    add ability to specify local chrome for testing

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 import puppeteer from 'puppeteer'
+import dotenv from 'dotenv'
 
 import mockServer from './src/index'
 
+dotenv.config()
+
+const executablePath = process.env.CHROME_PATH
+
 test('inits normally with Puppeteer', async () => {
   const browser = await puppeteer.launch({
+    executablePath,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   })
   const page = await browser.newPage()
